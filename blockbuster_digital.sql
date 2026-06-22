11f75163798a7f98d8acf508c7f9816ae87d4a4b
Maykel Tvihaug <maytvi33@gmail.com>
2026-06-22 03:21:50 -0300

    actualizacion de la bdd

--- blockbuster_digital.sql
+++ blockbuster_digital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-04-2026 a las 04:58:47
+-- Tiempo de generación: 22-06-2026 a las 08:20:38
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -39,28 +39,8 @@ CREATE TABLE `alquileres` (
 --
 
 INSERT INTO `alquileres` (`id_alquiler`, `id_pelicula`, `id_usuario`, `fecha_alquiler`) VALUES
-(1, 1, 2, '2026-04-19');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `generos`
---
-
-CREATE TABLE `generos` (
-  `id_genero` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `generos`
---
-
-INSERT INTO `generos` (`id_genero`, `nombre`) VALUES
-(1, 'Acción'),
-(2, 'Terror'),
-(3, 'Ciencia Ficción'),
-(4, 'Suspenso');
+(3, 5, 9, '2026-05-24'),
+(15, 11, 2, '2026-06-22');
 
 -- --------------------------------------------------------
 
@@ -72,9 +52,9 @@ CREATE TABLE `peliculas` (
   `id_pelicula` int(11) NOT NULL,
   `titulo` varchar(250) NOT NULL,
   `director` varchar(250) NOT NULL,
-  `id_genero` int(11) NOT NULL,
-  `alquilada` tinyint(1) NOT NULL DEFAULT 0,
-  `imagen` varchar(255) DEFAULT NULL
+  `id_genero` int(11) DEFAULT NULL,
+  `alquilada` tinyint(1) NOT NULL,
+  `imagen` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -82,10 +62,12 @@ CREATE TABLE `peliculas` (
 --
 
 INSERT INTO `peliculas` (`id_pelicula`, `titulo`, `director`, `id_genero`, `alquilada`, `imagen`) VALUES
-(1, 'Harry Potter y la Piedra Filosofal', 'Chris Columbus', 3, 1, ''),
-(2, 'The Shining', 'Stanley Kubrick', 2, 0, ''),
-(3, 'Inception', 'Christopher Nolan', 3, 0, ''),
-(4, 'Mad Max', 'George Miller', 1, 0, '');
+(2, 'The Shining', 'Stanley Kubrick', NULL, 1, ''),
+(3, 'Mad Max: Fury Road', 'George Miller', NULL, 1, ''),
+(4, 'The Matrix', 'Lana Wachowski', NULL, 1, ''),
+(5, 'Gladiator', 'Ridley Scott', 1, 0, ''),
+(6, 'Pulp Fiction', 'Quentin Tarantino', NULL, 1, ''),
+(11, 'Jurassic Park', 'Steven Spielberg', 1, 1, '');
 
 -- --------------------------------------------------------
 
@@ -105,8 +87,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `usuario`, `password`, `admin`) VALUES
-(1, 'admin@blockbuster.com', '$2y$10$/YpAqT8vPDIZNnZkx07zR.dpKpMPW.99dIqCiizQdd2q56il6ADkm', 1),
-(2, 'cliente@gmail.com', '$2y$10$9boEIiTw.s37isRw8csXP.gNdh9ZahfoQRPn/qJMwsxqrCSZzINy2', 0);
+(2, 'admin@blockbuster.com', '$2y$10$/YpAqT8vPDIZNnZkx07zR.dpKpMPW.99dIqCiizQdd2q56il6ADkm', 1),
+(9, 'cliente@gmail.com', '$2y$10$rYM5gxjKIEGxL9qkvuEFceTDNbUXVWgYgzIuigBSYLJiriRFwbcrS', 0);
 
 --
 -- Índices para tablas volcadas
@@ -117,21 +99,15 @@ INSERT INTO `usuarios` (`id_usuario`, `usuario`, `password`, `admin`) VALUES
 --
 ALTER TABLE `alquileres`
   ADD PRIMARY KEY (`id_alquiler`),
-  ADD UNIQUE KEY `id_pelicula_unica` (`id_pelicula`),
+  ADD UNIQUE KEY `id_pelicula_2` (`id_pelicula`),
+  ADD KEY `id_pelicula` (`id_pelicula`),
   ADD KEY `id_usuario` (`id_usuario`);
-
---
--- Indices de la tabla `generos`
---
-ALTER TABLE `generos`
-  ADD PRIMARY KEY (`id_genero`);
 
 --
 -- Indices de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
-  ADD PRIMARY KEY (`id_pelicula`),
-  ADD KEY `id_genero` (`id_genero`);
+  ADD PRIMARY KEY (`id_pelicula`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -147,25 +123,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `alquileres`
 --
 ALTER TABLE `alquileres`
-  MODIFY `id_alquiler` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `generos`
---
-ALTER TABLE `generos`
-  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_alquiler` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
-  MODIFY `id_pelicula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pelicula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restricciones para tablas volcadas
@@ -177,12 +147,6 @@ ALTER TABLE `usuarios`
 ALTER TABLE `alquileres`
   ADD CONSTRAINT `alquileres_ibfk_1` FOREIGN KEY (`id_pelicula`) REFERENCES `peliculas` (`id_pelicula`) ON DELETE CASCADE,
   ADD CONSTRAINT `alquileres_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `peliculas`
---
-ALTER TABLE `peliculas`
-  ADD CONSTRAINT `peliculas_ibfk_1` FOREIGN KEY (`id_genero`) REFERENCES `generos` (`id_genero`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
